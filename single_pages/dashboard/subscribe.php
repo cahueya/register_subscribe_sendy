d64ef3205688c9452686639549e8753ac516488a
cahueya <cahueya@users.noreply.github.com>
2019-02-09 12:21:12 +0100

    Update subscribe.php

--- single_pages/dashboard/subscribe.php
+++ single_pages/dashboard/subscribe.php
@@ -9,25 +9,29 @@ $ci = Loader::helper('concrete/ui');
 
 
 <div class="container">
+<div class="row">
 
 
 
 <form method="post" action="<?= $view->action('update_configuration')?>">
-    <fieldset><legend><?php echo t('Subscription Data'); ?></legend>
-        
-    <div class="col-xs-12 col-md-6">        
+		<?php
+        /** @var $token \Concrete\Core\Validation\CSRF\Token */
+        echo $token->output('perform_update_configuration');
+        ?>
+
+    <fieldset>
+     <div class="col-xs-12 col-md-8">     
+    <legend><?php echo t('Subscription Data'); ?></legend>           
         <div class="form-group">
             <label for="API_KEY"><?php echo t('Your API Key'); ?></label>
             <?php echo $form->text('API_KEY', $API_KEY, array('class' => 'span2', 'placeholder'=>t('API KEY')))?>
         </div>
-    </div>        
-    <div class="col-xs-12 col-md-6">    
+    
         <div class="form-group">
             <label for="INSTALL_URL"><?php echo t('The URL of your Sendy Installation'); ?></label>
             <?php echo $form->text('INSTALL_URL', $INSTALL_URL, array('class' => 'span2', 'placeholder'=>t('Install URL')))?>
         </div>
-    </div>        
-    <div class="col-xs-12 col-md-6">    
+    
         <div class="form-group">
             <label for="LIST_ID"><?php echo t('The List to which you want to subscribe'); ?></label>
             <?php echo $form->text('LIST_ID', $LIST_ID, array('class' => 'span2', 'placeholder'=>t('List ID')))?>
@@ -47,3 +51,4 @@ $ci = Loader::helper('concrete/ui');
         </div>
     </div>
 </form>
+</div>
